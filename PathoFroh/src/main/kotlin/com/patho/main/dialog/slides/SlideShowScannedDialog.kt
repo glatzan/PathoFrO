13bb9a3f432718f18ce1ec78b6ba3942f953bdf4
Andreas Glatz <glatzan@gmail.com>
2020-02-13 10:25:51 +0100

    show scanned slides dialog

--- PathoFroh/src/main/kotlin/com/patho/main/dialog/slides/SlideShowScannedDialog.kt
+++ PathoFroh/src/main/kotlin/com/patho/main/dialog/slides/SlideShowScannedDialog.kt
@@ -2,9 +2,32 @@ package com.patho.main.dialog.slides
 
 import com.patho.main.common.Dialog
 import com.patho.main.dialog.AbstractTaskDialog
+import com.patho.main.model.patient.Task
+import com.patho.main.model.scanner.ScannedSlide
+import com.patho.main.model.scanner.ScannedTask
+import com.patho.main.repository.jpa.ScannedTaskRepository
 import org.springframework.stereotype.Component
 
 @Component()
-class SlideShowScannedDialog : AbstractTaskDialog(Dialog.SLIDE_SHOW_SCANNED) {
+class SlideShowScannedDialog constructor(
+        private val scannedTaskRepository: ScannedTaskRepository) : AbstractTaskDialog(Dialog.SLIDE_SHOW_SCANNED) {
+
+    lateinit var scannedTask: ScannedTask
+
+    var selectedScannedSlide: ScannedSlide? = null
+
+    val isScannedSlideSelected
+        get() = selectedScannedSlide != null
+
+    /**
+     * Initializes the dialog
+     */
+    override fun initBean(task: Task): Boolean {
+        val result = super.initBean(task)
+
+        val scannedTask = scannedTaskRepository.findById(task.id).orElse(ScannedTask(task))
+
+        return result
+    }
 
 }
